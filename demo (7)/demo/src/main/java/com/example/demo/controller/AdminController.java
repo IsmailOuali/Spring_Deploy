a1b3d5dab39ad086f138b82a02a6373ccbe02d24
unknown <ismail.ouali@ump.ac.ma>
2024-12-24 09:19:06 +0100

    Changing the controllers

--- demo (7)/demo/src/main/java/com/example/demo/controller/AdminController.java
+++ demo (7)/demo/src/main/java/com/example/demo/controller/AdminController.java
@@ -1,40 +1,15 @@
-package com.example.demo.controller;
+package com.example.controller;
 
-
-import com.example.demo.dto.UtilisateurDto;
-import com.example.demo.entity.Role;
-import com.example.demo.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
-    @Autowired
-    private UserService userService;
 
-
-    @GetMapping("/users")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<List<UtilisateurDto>> getAllUtilisateurs() {
-        List<UtilisateurDto> utilisateurs = userService.getAllUtilisateurs()
-                .stream()
-                .map(UtilisateurDto::toDto)
-                .toList();
-        return ResponseEntity.ok(utilisateurs);
-    }
-
-
-    @PutMapping("/users/{userId}/role")
-    @PreAuthorize("hasRole('ADMIN')")
-    public ResponseEntity<UtilisateurDto> changeUserRole(
-            @PathVariable Long userId,
-            @RequestBody Role newRole) {
-        UtilisateurDto updatedUser = userService.changeUserRole(userId, newRole);
-        return ResponseEntity.ok(updatedUser);
+    @GetMapping("/dashboard")
+    public String getAdminDashboard() {
+        return "Welcome to the admin dashboard!";
     }
 }
